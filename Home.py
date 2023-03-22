543f1803b7c41be0514e2eddea2e21fce078e3ad
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-03-22 11:20:29 +0700

    .commit

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-#st.header("Kairungxxx")
+#st.header("Kairungxx")
 
 html_8 = """
 <div style="background-color:#99FF99;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
